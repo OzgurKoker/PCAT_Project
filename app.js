23e1111b65c902d99d9d1c2eb494a99d4bb67b60
Özgür Köker <ozgurkoker@Ozgur-MacBook-Pro.local>
2024-10-09 17:47:18 +0300

    Server hosted

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('hello');
-};
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo Description',
+  };
+  res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
