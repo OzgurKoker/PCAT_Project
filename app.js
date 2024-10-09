c4e647f7bb1e1f4c3b7d06d9bf370484f5154693
Özgür Köker <ozgurkoker@Ozgur-MacBook-Pro.local>
2024-10-09 18:10:16 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-
 const port = 3000;
 
+const myLogger = (req, res, next) => {
+  console.log('Middleware log 1');
+  next();
+};
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(myLogger);
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo Description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/PCAT_Starter_Files/index.html'));
 });
 
 app.listen(port, () => {
