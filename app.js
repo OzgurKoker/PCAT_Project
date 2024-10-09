9a3eda31570b8f7f010edd3610f67cb45fd98ef9
Özgür Köker <ozgurkoker@Ozgur-MacBook-Pro.local>
2024-10-09 19:32:08 +0300

    template engine created

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware log 1');
-  next();
-};
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(myLogger);
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/PCAT_Starter_Files/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
